356f4964f7de4fe75771a99037afd3a5af32ded7
unknown <jonathan jory@DESKTOP-GJOE1PR.DOMAINERURALES.LOCAL>
2023-12-19 10:34:29 +0100

    3eme angle

--- coc.py
+++ coc.py
@@ -89,3 +89,13 @@ else:
         print('left')
     else:
         print('right')
+
+"""
+calculer le troisième angle d'un triangle connaissant les deux premiers angles
+"""
+a, b = [int(i) for i in input().split()]
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+print(180 - a - b)
