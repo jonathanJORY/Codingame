2410aec07d8d9fe9740bc95feb17f1d1aaae03d7
unknown <jonathan jory@DESKTOP-GJOE1PR.DOMAINERURALES.LOCAL>
2023-12-19 09:23:48 +0100

    coc init

---
+++ coc.py
@@ -0,0 +1,26 @@
+"""
+Répétez le mot-clé jusqu'à ce qu'il atteigne la longueur du message.
+Message : CODINGAME
+Mot-clé : APPLEAPPL
+
+Additionnez le message et le mot-clé lettre par lettre en attribuant des valeurs entières aux lettres de 0 à 25 dans l'ordre alphabétique 
+(c'est-à-dire A=0, B=1, C=2, etc.)."""
+*l,k=open(0).read().split()
+a,b,*l=map(int,l)
+k*=a
+for i,j in zip(l,k):print(end=chr(65+(i-ord(j)+65)%26))
+
+"""renvoyer la moyenne des valeurs ascii des entrées"""
+s=[ord(a) for a in input()]
+print(sum(s)//len(s))
+
+"""
+Mettre en majuscule la première lettre de chaque mot et en minuscule les lettres suivantes."""
+
+import sys
+s = input()
+# Diviser la chaîne en mots
+mots = s.split()
+# Appliquer les modifications à chaque mot
+resultat = ' '.join([mot.capitalize() for mot in mots])
+print(resultat)
